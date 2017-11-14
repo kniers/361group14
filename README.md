76938d0451797d4fd293226fd9c3fdd938271d71
Steven Knier <kniers@flip3.engr.oregonstate.edu>
2017-11-13 20:19:49 -0800

    Initial content

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # 361group14
 Carpooling app
+
+Instructions for developers:
+clone git repo to your account on the OSU flip server
+run command:
+	git clone https://github.com/kniers/361group14
+
+Install the node packages with the command:
+	npm install
+	
+To start the server for development, run:
+	node server.js
+If the port is taken, edit the file server.js to pick a new port
+	app.set('port', 7526);  <- this is the line to change
+	
+To visit the site, you must be on the OSU VPN. See http://oregonstate.edu/helpdocs/network-and-phone/virtual-private-network-vpn
+Then, use a browser to navigate to flip3.engr.oregonstate.edu:7526
+If you are on a different flip number or different port, be sure to change them.
+
+
+In case you haven't used git in a while,
+you can stage files with:
+	git add filename
+or to stage all changes:
+	git add -A
+Then you commit your changes with:
+	git commit -m "My commit message. This is what I changed..."
+Finally, push your changes to the github repo:
+	git push origin
---
+++ server.js
@@ -0,0 +1,45 @@
+var express = require('express');
+
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var request = require('request');
+
+app.use(bodyParser.urlencoded({extended: false }));
+app.use(session({secret:'mySecretPassword'}));
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 7526);
+app.use(express.static('public'));
+
+app.get('/',function(req,res){
+	res.render('home');
+});
+
+app.get('/signup/',function(req,res){
+	res.render('signup');
+});
+
+app.get('/signin/',function(req,res){
+	res.render('signin');
+});
+
+app.get('/favicon.ico', function(req, res) {
+	res.sendStatus(204);
+});
+
+app.use(function(req,res){
+	res.status(404);
+	res.render('404');
+});
+
+app.use(function(err, req, res, next){
+	console.error(err.stack);
+	res.status(500);
+	res.render('500');
+});
+
+app.listen(app.get('port'),function(){
+	console.log('Express started on flip3.engr.oregonstate.edu:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
